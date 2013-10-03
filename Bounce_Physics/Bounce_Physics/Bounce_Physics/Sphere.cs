8d769799cc99421362fcb6a692644d1692c12da6
wtarr <william.projects.2012@gmail.com>
2013-10-04 00:42:05 +0100

    Without testing, willing to hazard a guess that time correction is working.

--- Bounce_Physics/Bounce_Physics/Bounce_Physics/Sphere.cs
+++ Bounce_Physics/Bounce_Physics/Bounce_Physics/Sphere.cs
@@ -18,6 +18,7 @@ namespace Bounce_Physics
     public class Sphere : GameObject, IamASphere
     {
         private Game _game;
+        private Vector3 previousPosition;
 
         public Sphere(Game game, Camera camera, Vector3 position, Vector3 velocity, float mass)
             : base(game, camera)
@@ -26,6 +27,7 @@ namespace Bounce_Physics
             _game = game;
             Velocity = velocity;
             Position = position;
+            BoundingSphere.Center = Position;
             Mass = mass;
         }
 
@@ -59,12 +61,21 @@ namespace Bounce_Physics
         public override void Update(GameTime gameTime)
         {
             // TODO: Add your update code here
-            var s = gameTime.TotalGameTime.Milliseconds/1000.0f;
-            Position += Velocity*gameTime.ElapsedGameTime.Milliseconds/1000.0f;
+            
+            
+            HasCollisionOccured(gameTime);
 
+            previousPosition = Position;
+
+            Position += Velocity * gameTime.ElapsedGameTime.Milliseconds / 1000.0f;
+            
             BoundingSphere.Center = Position;
 
-            HasCollisionOccured(gameTime);
+            //BoundingSphere.Radius = 5f;
+
+
+
+
 
             base.Update(gameTime);
         }
@@ -76,16 +87,17 @@ namespace Bounce_Physics
 
         public bool HasCollisionOccured(GameTime gt)
         {
-            foreach (var sphere in BounceyBounceGame.CollidableList)
-            {
-                Sphere s = sphere as Sphere;
-                if (this.BoundingSphere.Intersects(s.BoundingSphere) && s!= this)
+           
+                foreach (var sphere in BounceyBounceGame.CollidableList)
                 {
-                    s.defuseColor = new Vector3(255, 0, 0);
-                    CalculateFinalVelocity(this, s, gt);
+                    Sphere s = sphere as Sphere;
+                    if (BoundingSphere.Intersects(s.BoundingSphere) && !s.Equals(this))
+                    {
+                        s.defuseColor = new Vector3(255, 0, 0);
+                        ReactToCollison(this, s, gt);
+                    }
                 }
-            }
-
+            
             return false;
         }
 
@@ -95,27 +107,34 @@ namespace Bounce_Physics
             return me.Position + me.BoundingSphere.Radius*direction;
         }
         
-        public void CalculateFinalVelocity( Sphere s1, Sphere s2, GameTime gt)
+        public void ReactToCollison( Sphere s1, Sphere s2, GameTime gt)
+        {
+            
+
+            CounterOverLap(s1, s2, gt);
+
+            //CalcFinalVelocity(s1, s2);
+        }
+
+        private void CalcFinalVelocity(Sphere s1, Sphere s2)
         {
             // http://studiofreya.com/blog/3d-math-and-physics/simple-sphere-sphere-collision-detection-and-collision-response/
 
             // V can be broken into (V.n)n + (V -(V.n)n) i.e Vperp + Vparallel
 
-            DetermineWhereCollisionOccurred(s1, s2, gt);
-            
             var normal = Vector3.Normalize(s2.Position - s1.Position);
-            
+
             var x1 = Vector3.Dot(s1.Velocity, normal); //V.N
 
-            var v1Parallel = normal * x1; // PARALLEL
+            var v1Parallel = normal*x1; // PARALLEL
 
             var v1Perpendicular = s1.Velocity - v1Parallel; // PERP
 
             var m1 = s1.Mass;
-            
+
             // The other sphere
             normal = normal*-1;
-            
+
             var x2 = Vector3.Dot(normal, s2.Velocity);
 
             var v2Parallel = normal*x2;
@@ -128,27 +147,43 @@ namespace Bounce_Physics
             //v perp + v para (doesnt change)
 
             s2.Velocity = (v1Parallel*((2*m1)/(m1 + m2)) + v2Parallel*((m2 - m1)/(m1 + m2))) + v2Perpendicular;
-            
         }
-
-        private void DetermineWhenActualCollisonOccurred(Sphere s1, Sphere s2, GameTime gt)
+        
+        private void CounterOverLap(Sphere s1, Sphere s2, GameTime gt)
         {
-            // where are they now
-            // based on velocity 
-        }
 
-        private void DetermineWhereCollisionOccurred(Sphere s1, Sphere s2, GameTime gt)
-        {
-            // Determine over lap
-            var overlap = Vector3.Distance(s1.Position, s2.Position);
-            
-            var gcd = GreatestCommonDenominator(s1.Velocity.Length(), s2.Velocity.Length());
+            var overlap = (s1.BoundingSphere.Radius + s2.BoundingSphere.Radius) - Vector3.Distance(s1.Position, s2.Position);
 
-            var s1Portion = s1.Velocity.Length()/gcd;
-            var s2Portion = s2.Velocity.Length()/gcd;
+            var relVelocity = s1.Velocity - s2.Velocity;
 
-            var s1Adjusted = (overlap/(s1Portion + s2Portion))*s1Portion;
-            var s2Adjusted = (overlap/(s1Portion + s2Portion))*s2Portion;
+            var u = relVelocity.Length();
+
+            var t = overlap/u;
+
+            var timeOfCollision = gt.ElapsedGameTime.Milliseconds/1000.0f - t;
+
+            s1.Position = s1.previousPosition + s1.Velocity*timeOfCollision;
+            s2.Position = s2.previousPosition + s2.Velocity*timeOfCollision;
+            var distTest = Vector3.Distance(s1.Position, s2.Position);
+
+            CalcFinalVelocity(s1, s2);
+
+            // Catch up to current time
+
+            var diff = gt.ElapsedGameTime.Milliseconds/1000.0f - timeOfCollision;
+
+            s1.Position += s1.Velocity*diff;
+            s2.Position += s2.Velocity*diff;
+
+            //// Determine over lap
+            //var overlap = Math.Abs((s1.BoundingSphere.Radius + s2.BoundingSphere.Radius) - Vector3.Distance(s1.Position, s2.Position));
+
+            //var gcd = GreatestCommonDenominator(s1.Velocity.Length(), s2.Velocity.Length());
+            //var s1Portion = s1.Velocity.Length()/gcd;
+            //var s2Portion = s2.Velocity.Length()/gcd;
+
+            //var s1Adjusted = (overlap/(s1Portion + s2Portion))*s1Portion;
+            //var s2Adjusted = (overlap/(s1Portion + s2Portion))*s2Portion;
 
         }
 
